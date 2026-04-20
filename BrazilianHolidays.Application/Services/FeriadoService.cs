fc9748bbc44a33c3feda9600caa938b355c655a8
Rafael Santana <rafa03santana@gmail.com>
2026-04-20 08:16:43 -0300

    feat: adiciona 'FeriadoService'

---
+++ BrazilianHolidays.Application/Services/FeriadoService.cs
@@ -0,0 +1,93 @@
+﻿using BrazilianHolidays.Application.DTOs;
+using BrazilianHolidays.Domain.Enums;
+using BrazilianHolidays.Domain.Interfaces;
+
+namespace BrazilianHolidays.Application.Services;
+
+public class FeriadoService
+{
+    private readonly IFeriadoRepository _repository;
+
+    public FeriadoService(IFeriadoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<FeriadoDto>> ObterTodosPorAnoAsync(int ano)
+    {
+        var feriados = await _repository.ObterTodosPorAnoAsync(ano);
+        return feriados.Select(f => new FeriadoDto
+        {
+            Nome = f.Nome,
+            Data = f.Data,
+            Tipo = f.Tipo,
+            UF = f.UF,
+            Descricao = f.Descricao
+        });
+    }
+
+    public async Task<IEnumerable<FeriadoDto>> ObterNacionaisAsync(int ano)
+    {
+        var feriados = await _repository.ObterPorAnoETipoAsync(ano, TipoFeriado.Nacional);
+        return feriados.Select(f => new FeriadoDto
+        {
+            Nome = f.Nome,
+            Data = f.Data,
+            Tipo = f.Tipo,
+            Descricao = f.Descricao
+        });
+    }
+
+    public async Task<IEnumerable<FeriadoDto>> ObterPorUFAsync(int ano, string uf)
+    {
+        var feriados = await _repository.ObterPorAnoEUFAsync(ano, uf.ToUpper());
+        return feriados.Select(f => new FeriadoDto
+        {
+            Nome = f.Nome,
+            Data = f.Data,
+            Tipo = f.Tipo,
+            UF = f.UF,
+            Descricao = f.Descricao
+        });
+    }
+
+    public async Task<IEnumerable<FeriadoDto>> ObterPorMunicipioAsync(int ano, int codigoIbge)
+    {
+        var feriados = await _repository.ObterPorAnoEMunicipioAsync(ano, codigoIbge);
+        return feriados.Select(f => new FeriadoDto
+        {
+            Nome = f.Nome,
+            Data = f.Data,
+            Tipo = f.Tipo,
+            UF = f.UF,
+            Descricao = f.Descricao
+        });
+    }
+
+    public async Task<FeriadoDto?> ObterProximoFeriadoAsync()
+    {
+        var feriado = await _repository.ObterProximoFeriadoAsync();
+        if (feriado is null) return null;
+
+        return new FeriadoDto
+        {
+            Nome = feriado.Nome,
+            Data = feriado.Data,
+            Tipo = feriado.Tipo,
+            UF = feriado.UF,
+            Descricao = feriado.Descricao
+        };
+    }
+
+    public async Task<IEnumerable<FeriadoDto>> ObterPontosFacultativosAsync(int ano)
+    {
+        var feriados = await _repository.ObterPorAnoETipoAsync(ano, TipoFeriado.PontoFacultativo);
+        return feriados.Select(f => new FeriadoDto
+        {
+            Nome = f.Nome,
+            Data = f.Data,
+            Tipo = f.Tipo,
+            Descricao = f.Descricao
+        });
+    }
+}
