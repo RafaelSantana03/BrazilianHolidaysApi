f17147621e808c6c5e402d7e0da57cfb5d53ed13
Rafael Santana <rafa03santana@gmail.com>
2026-04-19 23:11:30 -0300

    feat: adiciona o AppDbContext

---
+++ BrazilianHolidays.Data/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using BrazilianHolidays.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrazilianHolidays.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {        
+    }
+    public DbSet<Feriado> Feriados { get; set; }
+}
