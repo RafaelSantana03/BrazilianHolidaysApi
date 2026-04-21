0515ae9304c2fa62e320b3a3d280dcf150e46f71
Rafael Santana <rafa03santana@gmail.com>
2026-04-21 03:02:19 -0300

    feat: integra com BrasilAPI para obtenção de feriados

---
+++ BrazilianHolidays.Data/DTOs/BrasilFeriadoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BrazilianHolidays.Data.DTOs;
+
+public class BrasilApiFeriadoDto
+{
+    public string Date { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
---
+++ BrazilianHolidays.Domain/Interfaces/IBrasilApiService.cs
@@ -0,0 +1,8 @@
+﻿using BrazilianHolidays.Domain.Entities;
+
+namespace BrazilianHolidays.Domain.Interfaces;
+
+public interface IBrasilApiService
+{
+    Task<IEnumerable<Feriado>> ObterFeriadosAsync(int ano);
+}
