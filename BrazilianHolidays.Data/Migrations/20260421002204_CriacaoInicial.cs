19ca5ab6697260d9924261b3ded2ef39cef4deb4
Rafael Santana <rafa03santana@gmail.com>
2026-04-20 21:40:31 -0300

    feat: cria estrutura inicial do banco com EF Core migrations

---
+++ BrazilianHolidays.Data/Migrations/20260421002204_CriacaoInicial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrazilianHolidays.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feriados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Data = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Tipo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Recorrente = table.Column<bool>(type: "INTEGER", nullable: false),
+                    UF = table.Column<string>(type: "TEXT", nullable: true),
+                    CodigoIbgeMunicipio = table.Column<int>(type: "INTEGER", nullable: true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feriados", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feriados");
+        }
+    }
+}
